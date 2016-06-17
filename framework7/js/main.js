3816f8d17578c7eea6f6df3287c0973aade02902
Richard Liu <richy.liu.2002@gmail.com>
2016-06-17 03:16:04 +0000

    try fix submodule problem

---
+++ framework7/js/main.js
@@ -0,0 +1,82 @@
+var myApp = new Framework7({
+    modalTitle: 'Framework7',
+    animateNavBackIcon: true,
+});
+
+// Expose Internal DOM library
+var $$ = Dom7;
+
+// Add main view
+var mainView = myApp.addView('.view-main', {
+    // Enable Dynamic Navbar for this view
+    dynamicNavbar: true,
+});
+
+$$('.pull-to-refresh-content').on('refresh', function(e) {
+    location.reload();
+});
+
+
+// Callbacks for specific pages when it initialized
+/* ===== Swipe to delete events callback demo ===== */
+myApp.onPageInit('swipe-delete', function (page) {
+    $$('.demo-remove-callback').on('deleted', function () {
+        myApp.alert('Thanks, item removed!');
+    });
+});
+myApp.onPageInit('swipe-delete media-lists', function (page) {
+    $$('.demo-reply').on('click', function () {
+        myApp.alert('Reply');
+    });
+    $$('.demo-mark').on('click', function () {
+        myApp.alert('Mark');
+    });
+    $$('.demo-forward').on('click', function () {
+        myApp.alert('Forward');
+    });
+});
+
+
+/* ===== Action sheet, we use it on few pages ===== */
+myApp.onPageInit('swipe-delete modals media-lists', function (page) {
+    var actionSheetButtons = [
+        // First buttons group
+        [
+            // Group Label
+            {
+                text: 'Here comes some optional description or warning for actions below',
+                label: true
+            },
+            // First button
+            {
+                text: 'Alert',
+                onClick: function () {
+                    myApp.alert('He Hoou!');
+                }
+            },
+            // Another red button
+            {
+                text: 'Nice Red Button ',
+                color: 'red',
+                onClick: function () {
+                    myApp.alert('You have clicked red button!');
+                }
+            },
+        ],
+        // Second group
+        [
+            {
+                text: 'Cancel',
+                bold: true
+            }
+        ]
+    ];
+    $$('.demo-actions').on('click', function (e) {
+        myApp.actions(actionSheetButtons);
+    });
+    $$('.demo-actions-popover').on('click', function (e) {
+        // We need to pass additional target parameter (this) for popover
+        myApp.actions(this, actionSheetButtons);
+    });
+
+});
