b33ead6087d6b28fe513c884a904a1b816ee2730
Richard Liu <richy.liu.2002@gmail.com>
2017-12-17 20:53:13 -0800

    update ballz
    this commit has been on my comp for a loooong time w/o pushing...

--- ballz/index.js
+++ ballz/index.js
@@ -9,8 +9,6 @@ for (let i = 0; i < 6; i++) {
 let balls = [];
 
 
-// how far to move each tick (about 60 times a second)
-const TICK_DISTANCE = 3;
 
 // size of each block in pixels
 const BLOCK_SIZE = 50;
@@ -29,170 +27,65 @@ const RELEASE_SPEED = 250;
 const RELEASE_X = 165;
 const RELEASE_Y = 430;
 
+// force at which to release the ball
+const RELEASE_FORCE = 0.001;
+
+
+// collision categories
+const DEFAULT_COLLISION = 0x0001;
 
 
 
-/**
- * A ball hits one block
- * @param {number} x X position of the block
- * @param {number} y Y position of the block
- */
-function hit(x, y) {
-    blocks[x][y]--;
-    // set block to not exist if it has 0 health
-    if (blocks[x][y] === 0) {
-        blocks[x][y] = -1;
+// module aliases
+let Engine = Matter.Engine,
+    Render = Matter.Render,
+    World = Matter.World,
+    Bodies = Matter.Bodies,
+    Events = Matter.Events,
+    Composites = Matter.Composites;
+
+// create an engine
+let engine = Engine.create();
+
+// create a renderer
+let render = Render.create({
+    element: document.body,
+    engine: engine,
+    options: {
+        width: 330,
+        height: 470
     }
-}
+});
 
 
-
-/**
- * Move all the balls, checking for hits and bounces
- */
-function moveBalls() {
-    balls.forEach(ball => {
-        // move the ball
-        ball[0] += Math.cos(ball[2]) * TICK_DISTANCE; // x coord
-        ball[1] += Math.sin(ball[2]) * TICK_DISTANCE; // y coord
-        
-        
-        // check for collision
-        // Modulo the position to get the ball's position within a square.
-        // Then it is easier to detect collisions (since everything else is a block)
-        
-        
-        // current x and y of the block the ball is on (using integer division)
-        let currentBlockX = Math.floor(ball[0] / TOTAL_BLOCK_SIZE);
-        let currentBlockY = Math.floor(ball[1] / TOTAL_BLOCK_SIZE);
-        // x position of the ball within the block
-        let blockX = ball[0] % TOTAL_BLOCK_SIZE;
-        let blockY = ball[1] % TOTAL_BLOCK_SIZE;
-        
-        // ensure ball is going left and check for collision left of block
-        if (Math.abs(ball[2]) > Math.PI*0.5 && blockX < BALL_RADIUS) {
-            // if left is wall
-            if (currentBlockX === 0) {
-                ball[2] = bounce(ball[2], 'left');
-                return;
-            } else if (blocks[currentBlockX - 1][currentBlockY] > 0) {
-                hit(currentBlockX - 1, currentBlockY);
-                ball[2] = bounce(ball[2], 'left');
-                return;
-            }
-        }
-        // ensure ball is going right and check for collision right of block
-        if (Math.abs(ball[2]) < Math.PI*0.5 && blockX > TOTAL_BLOCK_SIZE - BALL_RADIUS) {
-            // if right is wall
-            if (currentBlockX === 5) {
-                ball[2] = bounce(ball[2], 'right');
-                return;
-            } else if (blocks[currentBlockX + 1][currentBlockY] > 0) {
-                hit(currentBlockX + 1, currentBlockY);
-                ball[2] = bounce(ball[2], 'right');
-                return;
-            }
-        }
-        // ensure ball is going top and check for collision top of block
-        if (ball[2] < 0 && blockY < BALL_RADIUS) {
-            // if top is wall
-            if (currentBlockY === 0) {
-                ball[2] = bounce(ball[2], 'top');
-                return;
-            } else if (blocks[currentBlockX][currentBlockY - 1] > 0) {
-                hit(currentBlockX, currentBlockY - 1);
-                ball[2] = bounce(ball[2], 'top');
-                return;
-            }
-        }
-        // ensure ball is going bottom and check for collision bottom of block    
-        if (ball[2] > 0 && blockY > TOTAL_BLOCK_SIZE - BALL_RADIUS) {
-            // kill ball if bottom is wall
-            if (currentBlockY === 7) {
-                balls.splice(balls.indexOf(ball), 1);
-                return;
-            } else if (blocks[currentBlockX][currentBlockY + 1] > 0) {
-                hit(currentBlockX, currentBlockY + 1);
-                ball[2] = bounce(ball[2], 'bottom');
-                return;
-            }
-        }
-    });
-}
-
-
-
-/**
- * Bounce a ball off block/wall
- * @param  {number} direction The direction the ball is going
- * @param  {String} side      Where the block/wall is relative to the ball. Use
- *                            "left", "right", "top", or "bottom"
- * @return {number}           The direction the ball is now going.
- */
-function bounce(direction, side) {
-    // left and right are easy, just flip the sign of the direction
-    if (side == 'left' || side == 'right') {
-        if (direction < 0) {
-            return -Math.PI - direction;
-        } else {
-            return Math.PI - direction;
-        }
-    } else if (side == 'top' || side == 'bottom') {
-        return -direction;
-    } else {
-        console.error('Invalid direction to bounce in! (Direction: ' + direction + ')');
+// setup world constants
+engine.world.gravity.y = 0;
+engine.world.gravity.scale = 0;
+engine.world.bounds = {
+    min: {
+        x: -10,
+        y: -10
+    },
+    max: {
+        x: 1000,
+        y: 1000
     }
-}
+};
 
 
+let boundaryOpts = {
+    id: 'wall0',
+    isStatic: true,
+    restitution: 1,
+    frictionStatic: 0
+};
 
-/**
- * Draw the frame
- */
-function draw() {
-    moveBalls();
-    
-    let ctx = document.getElementById("canvas").getContext('2d');
-    
-    
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, WIDTH, HEIGHT);
-    
-    // draw blocks
-    for (let x = 0; x < 6; x++) {
-        for (let y = 0; y < 8; y++) {
-            if (blocks[x][y] > 0) {
-                ctx.fillStyle = 'blue';
-                ctx.fillRect(
-                    TOTAL_BLOCK_SIZE * x,
-                    TOTAL_BLOCK_SIZE * y,
-                    BLOCK_SIZE,
-                    BLOCK_SIZE
-                );
-                ctx.fillStyle = 'white';
-                ctx.font = '35px serif';
-                ctx.textAlign = 'center';
-                ctx.fillText(
-                    blocks[x][y],
-                    TOTAL_BLOCK_SIZE * x + BLOCK_SIZE/2,
-                    TOTAL_BLOCK_SIZE * y + BLOCK_SIZE * 5/6
-                );
-            }
-        }
-    }
-    
-    
-    // draw balls
-    ctx.fillStyle = 'white';
-    balls.forEach(ball => {
-        ctx.beginPath();
-        ctx.arc(ball[0], ball[1], BALL_RADIUS, 0, 2 * Math.PI, false);
-        ctx.fill();
-    });
-    
-    
-    if (!window.stopBallz) requestAnimationFrame(draw);
-}
+World.add(engine.world, [
+    Bodies.rectangle(-2.5, HEIGHT/2, 5, HEIGHT, boundaryOpts), // left wall
+    Bodies.rectangle(WIDTH+2.5, HEIGHT/2, 5, HEIGHT, { id: 'wall1', isStatic: true, restitution: 1, frictionStatic: 0 }), // right wall
+    Bodies.rectangle(WIDTH/2, -2.5, WIDTH, 5, { id: 'wall2', isStatic: true, restitution: 1, frictionStatic: 0 }), // top wall
+    Bodies.rectangle(WIDTH/2, HEIGHT+2.5, WIDTH, 5, { id: 'bottom', isStatic: true, restitution: 1, frictionStatic: 0 }), // bottom wall
+]);
 
 
 
@@ -205,9 +98,34 @@ function draw() {
  */
 function releaseBalls(amount, angle) {
     let count = 0;
+    balls = [];
+    
     let thisInterval = window.setInterval(() => {
         count++;
-        balls.push([RELEASE_X, RELEASE_Y, angle]);
+        
+        let ball = Bodies.circle(RELEASE_X, RELEASE_Y, BALL_RADIUS, {
+            force: {
+                x: Math.cos(angle) * RELEASE_FORCE,
+                y: Math.sin(angle) * RELEASE_FORCE
+            },
+            friction: 0,
+            frictionStatic: 0,
+            frictionAir: 0,
+            restitution: 1,
+            inertia: Infinity,
+            id: 'ba' + count,
+            render: {
+                fillStyle: 'blue'
+            },
+            collisionFilter: {
+                mask: DEFAULT_COLLISION,
+                category: 0x0002 // TODO: fix mask
+            }
+        });
+        balls.push(ball);
+
+        World.add(engine.world, ball);
+        
         if (count >= amount) {
             window.clearInterval(thisInterval);
         }
@@ -235,110 +153,32 @@ function nextLevel() {
 
 
 
-
-blocks[5][7] = 104;
-blocks[5][5] = 111;
-blocks[4][6] = 55;
-blocks[2][3] = 444;
-blocks[4][2] = 55;
-blocks[4][3] = 27;
-blocks[3][6] = 3;
-blocks[4][7] = 20;
-blocks[3][3] = 39;
-blocks[0][3] = 200;
-releaseBalls(10, Math.PI * -0.7);
-
-// start drawing the canvas
-// requestAnimationFrame(draw);
-
-
-
-
-// module aliases
-let Engine = Matter.Engine,
-    Render = Matter.Render,
-    World = Matter.World,
-    Bodies = Matter.Bodies,
-    Events = Matter.Events;
-
-// create an engine
-let engine = Engine.create();
-
-// create a renderer
-let render = Render.create({
-    element: document.body,
-    engine: engine,
-    options: {
-        width: 330,
-        height: 470
-    }
-});
-
-
-engine.world.gravity.y = 0;
-engine.world.gravity.scale = 0;
-engine.world.bounds = {
-    min: {
-        x: -10,
-        y: -10
-    },
-    max: {
-        x: 1000,
-        y: 1000
-    }
-};
-
-
-
-World.add(engine.world, [
-    Bodies.rectangle(-2.5, HEIGHT/2, 5, HEIGHT, { id: 'wall0', isStatic: true, restitution: 1, frictionStatic: 0 }), // left wall
-    Bodies.rectangle(WIDTH+2.5, HEIGHT/2, 5, HEIGHT, { id: 'wall1', isStatic: true, restitution: 1, frictionStatic: 0 }), // right wall
-    Bodies.rectangle(WIDTH/2, -2.5, WIDTH, 5, { id: 'wall2', isStatic: true, restitution: 1, frictionStatic: 0 }), // top wall
-    Bodies.rectangle(WIDTH/2, HEIGHT+2.5, WIDTH, 5, { id: 'bottom', isStatic: true, restitution: 1, frictionStatic: 0 }), // bottom wall
-]);
-
-// draw blocks
-for (let x = 0; x < 6; x++) {
-    for (let y = 0; y < 8; y++) {
-        if (blocks[x][y] > 0) {
-            // add all of the bodies to the world
-            // all x y coords refrence the center of the body
-            World.add(engine.world, Bodies.rectangle(
-                TOTAL_BLOCK_SIZE * x + TOTAL_BLOCK_SIZE/2,
-                TOTAL_BLOCK_SIZE * y + TOTAL_BLOCK_SIZE/2,
+function draw() {
+    World.add(engine.world,
+        Composites.stack(0, 0, 6, 8, BLOCK_SIZE, BLOCK_SIZE, (x, y, column, row) => {
+            if (blocks[column][row] < 0) return;
+            
+            return Bodies.rectangle(
+                x,
+                y,
                 BLOCK_SIZE,
                 BLOCK_SIZE,
                 {
                     isStatic: true,
+                    collisionFilter: {
+                        category: DEFAULT_COLLISION
+                    },
                     id: ('00'+ (x*10+y)).slice(-2), // x is first digits, y second
                     render: {
-                        fillStyle: '#245465'
+                        strokeStyle: 'blue'
                     }
                 }
-            ));
-        }
-    }
+            );
+        })
+    );
 }
 
-let ball = Bodies.circle(RELEASE_X, RELEASE_Y, BALL_RADIUS, {
-    force: {
-        x: -0.001,
-        y: -0.0015
-    },
-    friction: 0,
-    frictionStatic: 0,
-    frictionAir: 0,
-    restitution: 1,
-    inertia: Infinity,
-    id: 'ba0',
-    render: {
-        fillStyle: 'green'
-    },
-});
-balls = [];
-balls.push(ball);
 
-World.add(engine.world, ball);
 
 
 Events.on(engine, 'collisionEnd', event => {
@@ -354,8 +194,10 @@ Events.on(engine, 'collisionEnd', event => {
     // kill ball if hit bottom
     if (collisionIds[0].slice(0, 6) == 'bottom') {
         World.remove(engine.world, balls[collisionIds[1].slice(2)]);
+        return;
     } else if (collisionIds[1].slice(0, 6) == 'bottom') {
         World.remove(engine.world, balls[collisionIds[0].slice(2)]);
+        return;
     }
     
     
@@ -366,13 +208,33 @@ Events.on(engine, 'collisionEnd', event => {
         ballId = collisionIds[1].slice(2);
         coll = { x: parseInt(collisionIds[0].slice(0, 1)), y: parseInt(collisionIds[0].slice(1))};
     }
+    console.log(coll);
+    console.log(ballId);
     
-    
+    blocks[x][y]--;
+    if (blocks[x][y] === 0) {
+        blocks[x][y] = -1;
+    }
 });
 
 
+
+
+blocks[5][7] = 104;
+blocks[5][5] = 111;
+blocks[4][6] = 55;
+blocks[2][3] = 444;
+blocks[4][2] = 55;
+blocks[4][3] = 27;
+blocks[3][6] = 3;
+blocks[4][7] = 20;
+blocks[3][3] = 39;
+blocks[0][3] = 200;
+releaseBalls(10, Math.PI * -0.7);
+
+
+draw();
 // run the engine
 Engine.run(engine);
-
 // run the renderer
 Render.run(render);
